f493ef1148442fdbc10afa092c5323cfe0966e5f
Paul Konstantin Gerke <no@spam.com>
2017-01-08 00:18:37 +0100

    Changed: split out code over multiple files spearating PIC interface from the serial command interface for the Arduino Changed: renamed a few functions that had incorrect names Added: comments to the PIC progammer object Fixed: PIC programmer now allows 14 bits to be sent to the PIC instead of only 12 which is PIC10F206-specific (perhaps other but that is what it was designed for)

--- pic_programmer_prototype/pic_programmer_prototype.ino
+++ pic_programmer_prototype/pic_programmer_prototype.ino
@@ -2,35 +2,7 @@
 
 #include <StringSplitter.h>
 
-// These timings are all given in micro seconds
-#define PROGRAMMING_MODE_RESET_DELAY 20000           // Treset
-#define PROGRAMMING_MODE_VPP_LEAD_TIME 10            // Tppdp
-#define PROGRAMMING_MODE_INPUT_LINE_SET_TIME 1       // Tset0
-#define PROGRAMMING_MODE_HOLD_ACTIVATION_TIME 10     // Thld0
-#define PROGRAMMING_MODE_EXIT_DELAY_TIME 2           // Texit
-#define PROGRAMMING_MODE_PROGRAMMING_TIME 10000      // Tprog
-#define PROGRAMMING_MODE_PROGRAMMING_END_TIME 1000   // Tdis
-#define PROGRAMMING_MODE_ERASE_TIME 20000            // Tera
-
-#define PROGRAMMING_MODE_CLOCK_CYCLE_DELAY 1         // Tset1, Thld1
-#define PROGRAMMING_MODE_COMMAND_DELAY 5             // max(Tdly1, Tdly2, Tdly3)
-
-
-void ___customDelay(int x) {
-  x = 250;
-  //if (x < 0 || x > 1000) {
-  //  x = 1000;
-  //}
-  delay(x);
-}
-
-//#define micDelay(x) ___customDelay(x)
-#define micDelay(x) delayMicroseconds(x)
-
-const int POWER_PIN = 40;
-const int PROG_POWER_PIN = 41;
-const int PROGRAMMING_CLOCK_PIN = 42;
-const int PROGRAMMING_DATA_PIN = 43;
+#include "programmer.h"
 
 int hexToInt(const String& s) {
   const static char HEX_CHARS[17] = "0123456789abcdef";
@@ -52,180 +24,6 @@ int hexToInt(const String& s) {
   return result;
 }
 
-class PICProgrammer {
-  private:
-    int location;
-  
-    bool _hasPower, _inProgrammingMode;
-
-    void pushOutBits(int value, int count) {
-      for (int i = 0; i < count; i++) {
-        digitalWrite(PROGRAMMING_CLOCK_PIN, HIGH);
-        micDelay(PROGRAMMING_MODE_CLOCK_CYCLE_DELAY);
-        digitalWrite(PROGRAMMING_DATA_PIN, (value & 1) == 1 ? HIGH : LOW);
-        micDelay(PROGRAMMING_MODE_CLOCK_CYCLE_DELAY);
-        digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
-        micDelay(PROGRAMMING_MODE_CLOCK_CYCLE_DELAY);
-        value = (value >> 1) & 0x7FFF;
-      }
-    }
-
-    void writeCommand(unsigned char cmd) {
-      pushOutBits(cmd, 6);
-    }
-  public:
-    PICProgrammer() {
-      pinMode(PROGRAMMING_CLOCK_PIN, INPUT);
-      pinMode(PROGRAMMING_DATA_PIN, INPUT);
-    
-      pinMode(POWER_PIN, OUTPUT);
-      pinMode(PROG_POWER_PIN, OUTPUT);
-      digitalWrite(POWER_PIN, LOW);
-      digitalWrite(PROG_POWER_PIN, LOW);
-
-      _hasPower = false;
-      _inProgrammingMode = false;
-
-      // Wait for a general reset
-      delay(100);
-    }
-
-    int getLocation() {
-      return location;
-    }
-
-    bool hasPower() const {
-      return _hasPower;
-    }
-
-    bool inProgrammingMode() {
-      return _inProgrammingMode;
-    }
-
-    void setPower(bool on) {
-      if (_inProgrammingMode) {
-        setProgrammingMode(false);
-      }
-      if (on != _hasPower) {
-        digitalWrite(POWER_PIN, on ? HIGH : LOW);
-        _hasPower = on;
-        micDelay(PROGRAMMING_MODE_RESET_DELAY);
-      }
-    }
-
-    void setProgrammingMode(bool on) {
-      if (on != _inProgrammingMode) {
-        if (on) {
-          setPower(false);
-  
-          pinMode(PROGRAMMING_CLOCK_PIN, OUTPUT);
-          pinMode(PROGRAMMING_DATA_PIN, OUTPUT);
-  
-          digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
-          digitalWrite(PROGRAMMING_DATA_PIN, LOW);
-          micDelay(PROGRAMMING_MODE_INPUT_LINE_SET_TIME);
-  
-          digitalWrite(PROG_POWER_PIN, HIGH);
-          micDelay(PROGRAMMING_MODE_VPP_LEAD_TIME);
-          digitalWrite(POWER_PIN, HIGH);
-          micDelay(PROGRAMMING_MODE_HOLD_ACTIVATION_TIME);
-
-          location = -1;
-          _inProgrammingMode = true;
-          _hasPower = true;
-        } else {
-          pinMode(PROGRAMMING_DATA_PIN, OUTPUT);
-  
-          digitalWrite(PROGRAMMING_DATA_PIN, LOW);
-          digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
-          digitalWrite(POWER_PIN, LOW);
-          micDelay(PROGRAMMING_MODE_EXIT_DELAY_TIME);
-          
-          digitalWrite(PROG_POWER_PIN, LOW);
-          
-          micDelay(PROGRAMMING_MODE_RESET_DELAY);
-          
-          pinMode(PROGRAMMING_CLOCK_PIN, INPUT);
-          pinMode(PROGRAMMING_DATA_PIN, INPUT);
-          
-          _inProgrammingMode = false;
-          _hasPower = false;
-        }
-      }
-    }
-
-    int readWord(bool includeStartStopBits=false) {
-      if (_inProgrammingMode) {
-        writeCommand(0x04);
-
-        pinMode(PROGRAMMING_DATA_PIN, INPUT);
-        micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
-
-        int result = 0;
-        for (int i = 0; i < 16; i++) {
-          digitalWrite(PROGRAMMING_CLOCK_PIN, HIGH);
-          micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
-          result = (result >> 1) & 0x7FFF | (((bool) digitalRead(PROGRAMMING_DATA_PIN)) ? 0x8000 : 0);
-          digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
-          micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
-        }
-
-        pinMode(PROGRAMMING_DATA_PIN, OUTPUT);
-
-        if (includeStartStopBits) {
-          return result;
-        } else {
-          return (result >> 1) & 0x3FFF;
-        }
-      }
-      return -1;
-    }
-
-    bool writeWord(int w) {
-      w = (w & 0xFFF) << 1;
-      
-      if (_inProgrammingMode) {
-        writeCommand(0x02);
-        micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
-
-        pushOutBits(w, 16);
-        
-        digitalWrite(PROGRAMMING_DATA_PIN, LOW);
-        return true;
-      }
-      return false;
-    }
-
-    bool incAddress() {
-      if (_inProgrammingMode) {
-        writeCommand(0x06);
-        micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
-        location++;
-        return true;
-      }
-      return false;
-    }
-
-    bool burnProgram() {
-      if (_inProgrammingMode) {
-        writeCommand(0x08);
-        micDelay(PROGRAMMING_MODE_PROGRAMMING_TIME);
-        writeCommand(0x0E);
-        micDelay(PROGRAMMING_MODE_PROGRAMMING_END_TIME);
-        return true;
-      }
-      return false;
-    }
-    
-    bool erase() {
-      if (_inProgrammingMode) {
-        writeCommand(0x09);
-        micDelay(PROGRAMMING_MODE_ERASE_TIME);
-        return true;
-      }
-      return false;
-    }
-};
 
 PICProgrammer* programmer;
 
@@ -375,8 +173,8 @@ class WriteTextWordControl : public Command {
         parameter.toLowerCase();
         int data = hexToInt(parameter);
 
-        if (data < 0 || data > 0xFFF) {
-          Serial.println("ERROR: Word must be >= 0 and <= FFF");
+        if (data < 0 || data > 0x3FFF) {
+          Serial.println("ERROR: Word must be >= 0 and <= 3FFF");
         } else {
           programmer->writeWord(data);
         }
@@ -438,7 +236,7 @@ class BurnProgramControl : public Command {
       if (!programmer->inProgrammingMode()) {
         Serial.println("ERROR: Programmer not in programming mode");
       } else {
-        programmer->burnProgram();
+        programmer->burn();
       }
     }
     
@@ -501,7 +299,7 @@ class RewriteChipCommand : public Command {
             
             int w = programmer->readWord();
             programmer->writeWord(w);
-            programmer->burnProgram();
+            programmer->burn();
             
             programmer->incAddress();
             i++;
---
+++ pic_programmer_prototype/programmer.cpp
@@ -0,0 +1,205 @@
+#include "programmer.h"
+
+#include <Arduino.h>
+
+// These timings are all given in micro seconds
+#define PROGRAMMING_MODE_RESET_DELAY 20000           // Treset
+#define PROGRAMMING_MODE_VPP_LEAD_TIME 10            // Tppdp
+#define PROGRAMMING_MODE_INPUT_LINE_SET_TIME 1       // Tset0
+#define PROGRAMMING_MODE_HOLD_ACTIVATION_TIME 10     // Thld0
+#define PROGRAMMING_MODE_EXIT_DELAY_TIME 2           // Texit
+#define PROGRAMMING_MODE_PROGRAMMING_TIME 10000      // Tprog
+#define PROGRAMMING_MODE_PROGRAMMING_END_TIME 1000   // Tdis
+#define PROGRAMMING_MODE_ERASE_TIME 20000            // Tera
+
+#define PROGRAMMING_MODE_CLOCK_CYCLE_DELAY 1         // Tset1, Thld1
+#define PROGRAMMING_MODE_COMMAND_DELAY 5             // max(Tdly1, Tdly2, Tdly3)
+
+
+/* Wrapper to allow custom increases of the delay intervals for debugging purposes
+void ___customDelay(int x) {
+  x = 250;
+  //if (x < 0 || x > 1000) {
+  //  x = 1000;
+  //}
+  delay(x);
+}
+ */
+//#define micDelay(x) ___customDelay(x)
+
+#define micDelay(x) delayMicroseconds(x)
+
+
+const int POWER_PIN = 40;
+const int PROG_POWER_PIN = 41;
+const int PROGRAMMING_CLOCK_PIN = 42;
+const int PROGRAMMING_DATA_PIN = 43;
+
+void PICProgrammer :: pushOutBits(int value, int count) {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(PROGRAMMING_CLOCK_PIN, HIGH);
+    micDelay(PROGRAMMING_MODE_CLOCK_CYCLE_DELAY);
+    digitalWrite(PROGRAMMING_DATA_PIN, (value & 1) == 1 ? HIGH : LOW);
+    micDelay(PROGRAMMING_MODE_CLOCK_CYCLE_DELAY);
+    digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
+    micDelay(PROGRAMMING_MODE_CLOCK_CYCLE_DELAY);
+    value = (value >> 1) & 0x7FFF;
+  }
+}
+
+void PICProgrammer :: writeCommand(unsigned char cmd) {
+  pushOutBits(cmd, 6);
+}
+
+PICProgrammer :: PICProgrammer() {
+  pinMode(PROGRAMMING_CLOCK_PIN, INPUT);
+  pinMode(PROGRAMMING_DATA_PIN, INPUT);
+
+  pinMode(POWER_PIN, OUTPUT);
+  pinMode(PROG_POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, LOW);
+  digitalWrite(PROG_POWER_PIN, LOW);
+
+  _hasPower = false;
+  _inProgrammingMode = false;
+
+  // Wait for a general reset
+  delay(100);
+}
+
+int PICProgrammer :: getLocation() const {
+  return location;
+}
+
+bool PICProgrammer :: hasPower() const {
+  return _hasPower;
+}
+
+bool PICProgrammer :: inProgrammingMode() const {
+  return _inProgrammingMode;
+}
+
+void PICProgrammer :: setPower(bool on) {
+  if (_inProgrammingMode) {
+    setProgrammingMode(false);
+  }
+  if (on != _hasPower) {
+    digitalWrite(POWER_PIN, on ? HIGH : LOW);
+    _hasPower = on;
+    micDelay(PROGRAMMING_MODE_RESET_DELAY);
+  }
+}
+
+void PICProgrammer :: setProgrammingMode(bool on) {
+  if (on != _inProgrammingMode) {
+    if (on) {
+      setPower(false);
+
+      pinMode(PROGRAMMING_CLOCK_PIN, OUTPUT);
+      pinMode(PROGRAMMING_DATA_PIN, OUTPUT);
+
+      digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
+      digitalWrite(PROGRAMMING_DATA_PIN, LOW);
+      micDelay(PROGRAMMING_MODE_INPUT_LINE_SET_TIME);
+
+      digitalWrite(PROG_POWER_PIN, HIGH);
+      micDelay(PROGRAMMING_MODE_VPP_LEAD_TIME);
+      digitalWrite(POWER_PIN, HIGH);
+      micDelay(PROGRAMMING_MODE_HOLD_ACTIVATION_TIME);
+
+      location = -1;
+      _inProgrammingMode = true;
+      _hasPower = true;
+    } else {
+      pinMode(PROGRAMMING_DATA_PIN, OUTPUT);
+
+      digitalWrite(PROGRAMMING_DATA_PIN, LOW);
+      digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
+      digitalWrite(POWER_PIN, LOW);
+      micDelay(PROGRAMMING_MODE_EXIT_DELAY_TIME);
+      
+      digitalWrite(PROG_POWER_PIN, LOW);
+      
+      micDelay(PROGRAMMING_MODE_RESET_DELAY);
+      
+      pinMode(PROGRAMMING_CLOCK_PIN, INPUT);
+      pinMode(PROGRAMMING_DATA_PIN, INPUT);
+      
+      _inProgrammingMode = false;
+      _hasPower = false;
+    }
+  }
+}
+
+int PICProgrammer :: readWord(bool includeStartStopBits) {
+  if (_inProgrammingMode) {
+    writeCommand(0x04);
+
+    pinMode(PROGRAMMING_DATA_PIN, INPUT);
+    micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
+
+    int result = 0;
+    for (int i = 0; i < 16; i++) {
+      digitalWrite(PROGRAMMING_CLOCK_PIN, HIGH);
+      micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
+      result = (result >> 1) & 0x7FFF | (((bool) digitalRead(PROGRAMMING_DATA_PIN)) ? 0x8000 : 0);
+      digitalWrite(PROGRAMMING_CLOCK_PIN, LOW);
+      micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
+    }
+
+    pinMode(PROGRAMMING_DATA_PIN, OUTPUT);
+
+    if (includeStartStopBits) {
+      return result;
+    } else {
+      return (result >> 1) & 0x3FFF;
+    }
+  }
+  return -1;
+}
+
+bool PICProgrammer :: writeWord(int w) {
+  w = (w & 0x3FFF) << 1;
+  
+  if (_inProgrammingMode) {
+    writeCommand(0x02);
+    micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
+
+    pushOutBits(w, 16);
+    
+    digitalWrite(PROGRAMMING_DATA_PIN, LOW);
+    return true;
+  }
+  return false;
+}
+
+bool PICProgrammer :: incAddress() {
+  if (_inProgrammingMode) {
+    writeCommand(0x06);
+    micDelay(PROGRAMMING_MODE_COMMAND_DELAY);
+    location++;
+    return true;
+  }
+  return false;
+}
+
+bool PICProgrammer :: burn() {
+  if (_inProgrammingMode) {
+    writeCommand(0x08);
+    micDelay(PROGRAMMING_MODE_PROGRAMMING_TIME);
+    writeCommand(0x0E);
+    micDelay(PROGRAMMING_MODE_PROGRAMMING_END_TIME);
+    return true;
+  }
+  return false;
+}
+
+bool PICProgrammer :: erase() {
+  if (_inProgrammingMode) {
+    writeCommand(0x09);
+    micDelay(PROGRAMMING_MODE_ERASE_TIME);
+    return true;
+  }
+  return false;
+}
+
